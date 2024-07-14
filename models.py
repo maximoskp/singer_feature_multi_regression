a8f71686a063eb27bc7c370bfd024ffc61b64dbb
maximoskp <maximoskalpap@gmail.com>
2024-07-14 07:59:32 +0300

    before forward

--- models.py
+++ models.py
@@ -1,14 +1,17 @@
 # https://medium.com/@shahrukhx01/multi-task-learning-with-transformers-part-1-multi-prediction-heads-b7001cf014bf
 # https://discuss.huggingface.co/t/fine-tuning-bert-with-multiple-classification-heads/24453/8
 from transformers import Wav2Vec2Model, Wav2Vec2Config, HubertConfig, PretrainedConfig, AutoFeatureExtractor, AutoModel
+from transformers.modeling_outputs import SequenceClassifierOutput
 import torch
 from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+import numpy as np
 
 class HuBERTMultiHead(Wav2Vec2Model):
     def __init__(self,  **kwargs):
         super().__init__(Wav2Vec2Config())
-        
-        self.num_labels = kwargs.get('task_labels_map', {})
+        self.task_labels = kwargs.get('task_labels_map', {})
+        # self.num_tasks = len( self.task_labels ) # TODO: we don't need it
         # self.config = config
         self.projector_dim = 64
 
@@ -23,29 +26,22 @@ class HuBERTMultiHead(Wav2Vec2Model):
         # self.sampling_rate = self.feature_extractor.sampling_rate
         # self.hubert = AutoModel.from_pretrained(self.model_id)
 
-        ## add task specific output heads - TODO: make a list of linear
-        self.projector1 = nn.Linear(
-            self.base_model.config.hidden_size, self.projector_dim
-        )
-        self.classifier1 = nn.Linear(
-            self.projector_dim, list(self.num_labels.values())[0]
-        )
-        self.projector2 = nn.Linear(
-            self.base_model.config.hidden_size, self.projector_dim
-        )
-        self.classifier2 = nn.Linear(
-            self.projector_dim, list(self.num_labels.values())[1]
-        )
+        ## add task specific output heads
+        self.projectors = {}
+        self.classifiers = {}
+        for tl in self.task_labels.keys():
+            self.projectors[tl] = nn.Linear(
+                self.base_model.config.hidden_size, self.projector_dim
+            )
+            self.classifiers[tl] = nn.Linear(
+                self.projector_dim, self.task_labels[tl]
+            )
     # end init
     
     def forward(
             self,
         input_audios=None,
         attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        head_mask=None,
-        inputs_embeds=None,
         labels=None,
         output_attentions=None,
         output_hidden_states=None,
@@ -56,22 +52,65 @@ class HuBERTMultiHead(Wav2Vec2Model):
             return_dict if return_dict is not None else self.config.use_return_dict
         )
 
-        inputs = self.feature_extractor(
+        audio_normalized = self.feature_extractor(
             input_audios,
             sampling_rate=self.sampling_rate,
             return_attention_mask=True,
         )
-
+        
+        audio_tensors = torch.from_numpy(np.array(audio_normalized['input_values']))
+        attention_mask = torch.from_numpy(np.array(audio_normalized['attention_mask']))
+        
         outputs = self.hubert(
-            inputs,
+            audio_tensors,
             attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict
+        )
+
+        pooled_y = outputs['last_hidden_state'].mean(axis=1)
+
+        y = self.projectors[task_name]
+        logits = self.classifiers[task_name]
+
+        self.problem_type = None
+        loss = None
+        # if labels are given, i.e., if in training mode
+        if labels is not None:
+            # check the type of problem, i.e., regression or singe/multi classfication
+            if self.problem_type is None:
+                if self.num_labels[task_name] == 1:
+                    self.problem_type = "regression"
+                elif self.num_labels[task_name] > 1 and (
+                    labels.dtype == torch.long or labels.dtype == torch.int
+                ):
+                    self.problem_type = "single_label_classification"
+                else:
+                    self.problem_type = "multi_label_classification"
+        # apply loss
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels[task_name] == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(
+                    logits.view(-1, self.num_labels[task_name]), labels.view(-1)
+                )
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs['last_hidden_state'],
+            attentions=outputs.attentions
         )
     # end forward
 # end class
