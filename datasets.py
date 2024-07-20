ccab41c9217c0398c7742b064b8089b54dac5bd6
maximoskp <maximoskalpap@gmail.com>
2024-07-20 08:04:14 +0300

    close to full dataset

--- datasets.py
+++ datasets.py
@@ -3,6 +3,8 @@ import torch
 from torch.utils.data import Dataset
 from pathlib import Path
 import pandas as pd
+import librosa
+from copy import deepcopy
 
 class SingerMultiTaskDataset(Dataset):
     def __init__(self, audios_parent_path, csv_path, sampling_rate=16000):
@@ -14,6 +16,12 @@ class SingerMultiTaskDataset(Dataset):
         self.audio_paths = list( Path(self.audios_parent_path).glob('**/*.mp3') )
         # load csv
         self.feats = pd.read_csv(self.csv_path, delimiter=',')
+        # keep file names
+        self.names = self.feats['names']
+        # make a dict that keeps only number of output labels per task
+        # for defining regression or classification
+        task_labels_num = {k:1 for k in list(self.feats.columns)}
+        task_labels_num['singer_id'] = self.feats['singer_id'].max()+1
     # end init
 
     def __len__(self):
@@ -22,7 +30,12 @@ class SingerMultiTaskDataset(Dataset):
 
     def __getitem__(self, idx):
         # load audio with librosa in the desired sample rate as mono
-        # 
+        audio_var, _ = librosa.load(self.audio_paths[idx], sr=self.sampling_rate)
+        # get proper line values from csv
+        feats_line = self.feats[ self.feats['names'] == self.names[0] ]
+        # make a dict
+        feats_line_lists = feats_line.to_dict('tight')
+        feats_line_dict = dict( zip( feats_line_lists['columns'] , feats_line_lists['data'][0] ) )
         pass
     # end getitem
 # end class
